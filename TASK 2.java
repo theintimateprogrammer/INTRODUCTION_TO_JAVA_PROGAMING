b43149fd860f0f42b196a422a495854d98eb6b3c
theintimateprogrammer <112021626+theintimateprogrammer@users.noreply.github.com>
2023-01-19 11:46:25 +0530

    Add files via upload

---
+++ TASK 2.java
@@ -0,0 +1,37 @@
+public class student2{
+    public static void main(String[] args) {
+        int a = 1;
+        char b = 'd';
+        float c = 2.11f;public class dharm2 {
+    public static void main(String[] args) {
+        int a = 1;
+        char b = 'd';
+        float c = 2.11f;
+        double d = 1.22;
+        long e = 312;
+        String f = "deep patel";
+
+        System.out.println("Int= " + a);
+        System.out.println("Char= " + b);
+        System.out.println("Float= " + c);
+        System.out.println("Double= " + d);
+        System.out.println("Long= " + e);
+        System.out.println("String= " + f);
+
+    }
+
+}
+        double d = 1.22;
+        long e = 312;
+        String f = "James Gosling";
+
+        System.out.println("Int= " + a);
+        System.out.println("Char= " + b);
+        System.out.println("Float= " + c);
+        System.out.println("Double= " + d);
+        System.out.println("Long= " + e);
+        System.out.println("String= " + f);
+
+    }
+
+}
